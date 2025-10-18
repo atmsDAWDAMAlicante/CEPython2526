cb135891a83523cc4d50420c3ef8e04aa1933a05
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-10-18 18:50:15 +0200

    Diseño de la plantilla de los archivos para ejercicios

--- Modulo_1_Basico/EJERCICIOS_02_Modulo_01_Diccionarios.py
+++ Modulo_1_Basico/EJERCICIOS_02_Modulo_01_Diccionarios.py
@@ -1,10 +1,68 @@
 # EJERCICIOS - 02 - Modulo - 01 - Diccionarios
 # Diccionarios: Bloque de ejercicios progresivos. Es obligatorio incluir comentarios en todos ellos.
 
+import os
+
+linea = "===================================="
+os.system('cls')
+print(f"{linea}\nInicio de los ejercicios de este bloque\n{linea}")
 
 
+#1. Enunciado
 
 
+print(f"{linea}")
 
 
+#2. Enunciado
 
+
+print(f"{linea}")
+
+
+#3. Enunciado
+
+
+print(f"{linea}")
+
+
+#4. Enunciado
+
+
+print(f"{linea}")
+
+
+#5. Enunciado
+
+
+print(f"{linea}")
+
+
+#6. Enunciado
+
+
+print(f"{linea}")
+
+
+#7. Enunciado
+
+
+print(f"{linea}")
+
+
+#8. Enunciado
+
+
+print(f"{linea}")
+
+
+#9. Enunciado
+
+
+print(f"{linea}")
+
+
+#10. Enunciado
+
+
+print(f"{linea}\nFin de los ejercicios de este bloque\n{linea}")
--- Modulo_1_Basico/plantilla.py
+++ Modulo_1_Basico/plantilla.py
@@ -1,5 +1,4 @@
-# EJERCICIOS - 01- Modulo - 02 - Segunda semana
-# Ejercicios para la tutoría colectiva UD1 Segunda semana
+#NOMBRE DEL PDF DE EJERCICIOS
 
 import os
 
