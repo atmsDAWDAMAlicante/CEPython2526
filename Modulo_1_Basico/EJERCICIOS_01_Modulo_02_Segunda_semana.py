a13d62686de15df3a4e7923c259eaa82516bc496
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-10-18 19:12:27 +0200

    Diseño de la plantilla de los archivos para ejercicios 2

--- Modulo_1_Basico/EJERCICIOS_01_Modulo_02_Segunda_semana.py
+++ Modulo_1_Basico/EJERCICIOS_01_Modulo_02_Segunda_semana.py
@@ -8,61 +8,85 @@ os.system('cls')
 print(f"{linea}\nInicio de los ejercicios de este bloque\n{linea}")
 
 
-#1. Enunciado
+#1. Escribir un programa que pregunte el nombre completo del usuario en la consola 
+# y después muestre por pantalla el nombre completo del usuario tres veces, 
+# una con todas las letras minúsculas, otra con todas las letras mayúsculas 
+# y otra solo con la primera letra del nombre y de los apellidos en mayúscula. 
+# El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera.
+print(f"---Ejercicio nº 1: xxx")
+
 
 
 print(f"{linea}")
 
 
 #2. Enunciado
+print(f"---Ejercicio nº 2: xxx")
+
 
 
 print(f"{linea}")
 
 
 #3. Enunciado
+print(f"---Ejercicio nº 3: xxx")
+
 
 
 print(f"{linea}")
 
 
 #4. Enunciado
+print(f"---Ejercicio nº 4: xxx")
+
 
 
 print(f"{linea}")
 
 
 #5. Enunciado
+print(f"---Ejercicio nº 5: xxx")
+
 
 
 print(f"{linea}")
 
 
 #6. Enunciado
+print(f"---Ejercicio nº 6: xxx")
+
 
 
 print(f"{linea}")
 
 
 #7. Enunciado
+print(f"---Ejercicio nº 7: xxx")
+
 
 
 print(f"{linea}")
 
 
 #8. Enunciado
+print(f"---Ejercicio nº 8: xxx")
+
 
 
 print(f"{linea}")
 
 
 #9. Enunciado
+print(f"---Ejercicio nº 9: xxx")
+
 
 
 print(f"{linea}")
 
 
 #10. Enunciado
+print(f"---Ejercicio nº 10: xxx")
+
 
 
 print(f"{linea}\nFin de los ejercicios de este bloque\n{linea}")
--- Modulo_1_Basico/EJERCICIOS_02_Modulo_01_Diccionarios.py
+++ Modulo_1_Basico/EJERCICIOS_02_Modulo_01_Diccionarios.py
@@ -9,60 +9,80 @@ print(f"{linea}\nInicio de los ejercicios de este bloque\n{linea}")
 
 
 #1. Enunciado
+print(f"---Ejercicio nº 1: xxx")
+
 
 
 print(f"{linea}")
 
 
 #2. Enunciado
+print(f"---Ejercicio nº 2: xxx")
+
 
 
 print(f"{linea}")
 
 
 #3. Enunciado
+print(f"---Ejercicio nº 3: xxx")
+
 
 
 print(f"{linea}")
 
 
 #4. Enunciado
+print(f"---Ejercicio nº 4: xxx")
+
 
 
 print(f"{linea}")
 
 
 #5. Enunciado
+print(f"---Ejercicio nº 5: xxx")
+
 
 
 print(f"{linea}")
 
 
 #6. Enunciado
+print(f"---Ejercicio nº 6: xxx")
+
 
 
 print(f"{linea}")
 
 
 #7. Enunciado
+print(f"---Ejercicio nº 7: xxx")
+
 
 
 print(f"{linea}")
 
 
 #8. Enunciado
+print(f"---Ejercicio nº 8: xxx")
+
 
 
 print(f"{linea}")
 
 
 #9. Enunciado
+print(f"---Ejercicio nº 9: xxx")
+
 
 
 print(f"{linea}")
 
 
 #10. Enunciado
+print(f"---Ejercicio nº 10: xxx")
+
 
 
 print(f"{linea}\nFin de los ejercicios de este bloque\n{linea}")
