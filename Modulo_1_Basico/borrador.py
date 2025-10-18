6863ad46d150a0933512408df6b75fa2ae15f45f
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-10-18 12:40:36 +0200

    Notas en el borrador

---
+++ Modulo_1_Basico/borrador.py
@@ -0,0 +1,24 @@
+print("Hola mundo")
+a: int = 4
+b = "Hola"
+c = 4
+print(b+str(c))
+"""
+Comentario de
+ varias líneas
+"""
+print(abs(-3.1416))
+
+nom1 = "Don Pepito"
+nom2 = "Don José"
+lugar1 = "casa"
+persona1 = "abuela"
+print(f'Hola, {nom1}\nHola, {nom2}\n¿Pasó usted por mi {lugar1}?')
+
+valor1 = 1234567890
+print(f'{valor1:.2f}')
+print(f'{valor1:1d} - {valor1:2d}')
+
+divisionentera = divmod(342423,565)
+print(divisionentera) #(606, 33)
+print(pow(2,3))
