33f9aa0f4b1c32b81cd3166d3969dc1d7b8d49b9
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-10-19 18:05:28 +0200

    Redactada la plantilla del Módulo_02_Ejercicios_01 y tests

--- Modulo_1_Basico/ejercicios/EJERCICIOS_02_Modulo_01_Condicionales.py
+++ Modulo_1_Basico/ejercicios/EJERCICIOS_02_Modulo_01_Condicionales.py
@@ -13,11 +13,12 @@ print(f"{linea}\nInicio de los ejercicios de este bloque\n{linea}")
 # una con todas las letras minúsculas, otra con todas las letras mayúsculas 
 # y otra solo con la primera letra del nombre y de los apellidos en mayúscula. 
 # El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera.
-print(f"---Ejercicio nº 1: xxx")
+def ejercicio_01():
+    print(f"---Ejercicio nº 1: xxx")
 
 
 
-print(f"{linea}")
+    print(f"{linea}")
 
 
 #2. Los teléfonos de una empresa tienen el siguiente formato prefijo-número-extensión 
@@ -25,40 +26,44 @@ print(f"{linea}")
 # (por ejemplo +34-913724710-56). 
 # Escribir un programa que pregunte por un número de teléfono con este formato 
 # y muestre por pantalla el número de teléfono sin el prefijo y la extensión.
-print(f"---Ejercicio nº 2: xxx")
+def ejercicio_02():
+    print(f"---Ejercicio nº 2: xxx")
 
 
 
-print(f"{linea}")
+    print(f"{linea}")
 
 
 #3. Escribir un programa que pregunte al usuario la fecha de su nacimiento en formato dd/mm/aaaa 
 # y muestra por pantalla, el día, el nombre del mes y el año. 
 # Adaptar el programa anterior para que también funcione cuando el año se introduzca con 2 o 4 dígitos.
-print(f"---Ejercicio nº 3: xxx")
+def ejercicio_03():
+    print(f"---Ejercicio nº 3: xxx")
 
 
 
-print(f"{linea}")
+    print(f"{linea}")
 
 
 #4. Ampliar el programa anterior para que acepte tres idiomas.
-print(f"---Ejercicio nº 4: xxx")
+def ejercicio_04():
+    print(f"---Ejercicio nº 4: xxx")
 
 
 
-print(f"{linea}")
+    print(f"{linea}")
 
 
 #5. Escribir un programa que almacene la cadena de caracteres contraseña en una variable, 
 # pregunte al usuario por la contraseña e imprima por pantalla 
 # si la contraseña introducida por el usuario coincide con la guardada en la variable 
 # sin tener en cuenta mayúsculas y minúsculas.
-print(f"---Ejercicio nº 5: xxx")
+def ejercicio_05():
+    print(f"---Ejercicio nº 5: xxx")
 
 
 
-print(f"{linea}")
+    print(f"{linea}")
 
 
 #6. Los alumnos de un curso se han dividido en dos grupos A y B 
@@ -69,7 +74,8 @@ print(f"{linea}")
 # y # el grupo B por el resto. 
 # Escribir un programa que pregunte al usuario su nombre y edad, 
 # y muestre por pantalla el grupo que le corresponde.
-print(f"---Ejercicio nº 6: xxx")
+def ejercicio_06():
+    print(f"---Ejercicio nº 6: xxx")
 
 
 
--- Modulo_1_Basico/tests/test_EJERCICIOS_02_Modulo_01_Condicionales.py
+++ Modulo_1_Basico/tests/test_EJERCICIOS_02_Modulo_01_Condicionales.py
@@ -1,3 +1,23 @@
 import pytest
 import ejercicios.EJERCICIOS_02_Modulo_01_Condicionales as mod2_eje1
 
+
+def test_ejercicio_01():
+    pass
+    #assert mod2_eje1.ejercicio_01("Angel") == "Encantado de conocerte, Angel."
+
+
+def test_ejercicio_02():
+    pass
+
+def test_ejercicio_03():
+    pass
+
+def test_ejercicio_04():
+    pass
+
+def test_ejercicio_05():
+    pass
+
+def test_ejercicio_06():
+    pass
