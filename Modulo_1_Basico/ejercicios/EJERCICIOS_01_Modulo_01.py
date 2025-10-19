c9662aa6d19e094d9072628957f13de3e26b148f
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-10-19 16:45:10 +0200

    Completados los tests y funciones del Módulo 1 - Semana 1

--- Modulo_1_Basico/ejercicios/EJERCICIOS_01_Modulo_01.py
+++ Modulo_1_Basico/ejercicios/EJERCICIOS_01_Modulo_01.py
@@ -1,6 +1,7 @@
 # EJERCICIOS - 01- Modulo - 01
 # Ejercicios de Python Sintaxis
 import os
+import math
 
 linea = "===================================="
 os.system('cls')
@@ -8,7 +9,7 @@ print(f"{linea}\nInicio de los ejercicios de este bloque\n{linea}")
 
 
 #1. Pide al usuario su nombre completo y muestra el mensaje: Encantado de conocerte <nombre>
-def ejercicio1(nombre_completo):
+def ejercicio_01(nombre_completo):
     print(f"---Ejercicio nº 1: saludo")
     #nombre_completo = input("Introduce tu nombre completo: ")
     resultado_saludo = f'Encantado de conocerte, {nombre_completo}.'
@@ -20,11 +21,14 @@ def ejercicio1(nombre_completo):
 
 #2. Realiza un programa que introduciéndole los kilómetros/hora me calcule los metros/segundo.
 # Fórmula: hay que multiplicar por 1000 metros y dividir por 3600 segundos
-print(f"---Ejercicio nº 2: km/h a m/s")
-kilometros_hora = float(input("Introduce un número (Km/h): "))
-metros_segundo = ((kilometros_hora*1000)/(3600))
-print(f'{kilometros_hora} Km/h son {metros_segundo} Metros/segundo') # 45 Km/h = 12.5 m/s
-print(f"{linea}")
+def ejercicio_02(kilometros_hora):
+    print(f"---Ejercicio nº 2: km/h a m/s")
+    #kilometros_hora = float(input("Introduce un número (Km/h): "))
+    metros_segundo = round(((kilometros_hora*1000)/(3600)),2)
+    print(metros_segundo)
+    print(f'{kilometros_hora} Km/h son {metros_segundo} Metros/segundo') # 45 Km/h = 12.5 m/s
+    print(f"{linea}")
+    return metros_segundo
 
 
 #3. Solicita una cantidad a hipotecar y un interés anual (%), y calcula el pago anual con la fórmula: 
@@ -44,13 +48,14 @@ from math import sqrt
 Importaría solo la función sqrt() y se podría llamar directamente sqrt(9)
 
 """
-
-print(f"---Ejercicio nº 3: cuota de préstamo hipotecario")
-principal = float(input("Introduce la cantidad correspondiente al principal: "))
-interes = float(input("Introduce la cantidad correspondiente al interés %: "))
-pago_anual = principal * (interes/100)
-print(f'La cuota anual del préstamo hipotecario, (capital: {principal} € al {interes}% de interés) asciende a {pago_anual} €')
-print(f"{linea}")
+def ejercicio_03(principal, interes):
+    print(f"---Ejercicio nº 3: cuota de préstamo hipotecario")
+    #principal = float(input("Introduce la cantidad correspondiente al principal: "))
+    #interes = float(input("Introduce la cantidad correspondiente al interés %: "))
+    pago_anual = principal * (interes/100)
+    print(f'La cuota anual del préstamo hipotecario, (capital: {principal} € al {interes}% de interés) asciende a {pago_anual} €')
+    print(f"{linea}")
+    return pago_anual
 
 
 #4. Realiza un programa que calcule las raíces de un polinomio de 2º grado.
@@ -63,25 +68,26 @@ x = (-b +/- RAIZ CUADRADA de b2 - 4ac ) /2a
 
 Leer los valores a, b y c. Aplicad la fórmula. Recordad que tiene dos soluciones.
 """
-print(f"---Ejercicio nº 4: polinomio de 2º grado (funciona con 1-9-6)")
-# Introducción de los coeficientes a, b, c
-print("Introduce tres números float")
-numero_a = float(input("Introduce el primer coeficiente 'a': "))
-numero_b = float(input("Introduce el primer coeficiente 'b': "))
-numero_c = float(input("Introduce el primer coeficiente 'c': "))
+def ejercicio_04(numero_a, numero_b, numero_c):
+    print(f"---Ejercicio nº 4: polinomio de 2º grado (funciona con 1-9-6)")
+    # Introducción de los coeficientes a, b, c
+    print("Introduce tres números float")
+    #numero_a = float(input("Introduce el primer coeficiente 'a': "))
+    #numero_b = float(input("Introduce el primer coeficiente 'b': "))
+    #numero_c = float(input("Introduce el primer coeficiente 'c': "))
 
-# Cálculo de x
-# hay que importar un módulo
-import math
-#Cálculo de las dos x
-para_calcular_en_la_raiz = (pow(numero_b,2))- 4 * numero_a * numero_c
-divisor = 2 * numero_a
-numero_x1 = ((numero_b * -1) + math.sqrt(para_calcular_en_la_raiz)) / divisor
-numero_x2 = ((numero_b * -1) - math.sqrt(para_calcular_en_la_raiz)) / divisor
-print(f'El número x1 es: {numero_x1}')
-print(f'El número x2 es: {numero_x2}')
-print(f"{linea}")
-
+    # Cálculo de x
+    # hay que importar un módulo
+    import math
+    #Cálculo de las dos x
+    para_calcular_en_la_raiz = (pow(numero_b,2))- 4 * numero_a * numero_c
+    divisor = 2 * numero_a
+    numero_x1 = ((numero_b * -1) + math.sqrt(para_calcular_en_la_raiz)) / divisor
+    numero_x2 = ((numero_b * -1) - math.sqrt(para_calcular_en_la_raiz)) / divisor
+    print(f'El número x1 es: {numero_x1}')
+    print(f'El número x2 es: {numero_x2}')
+    print(f"{linea}")
+    return (numero_x1, numero_x2)
 
 #5. Realiza un programa que lea una temperatura en grados centígrados y la pase a grados Kelvin, Farenheit, grados cochinillos y grados alubianos.
 """
@@ -90,16 +96,18 @@ print(f"{linea}")
 ºCochinillos = ºC * 22 + 123 - 2 + ºC / ºK;
 ºAlubianos = (ºC - ºCochinillos + ºF) / ºK;
 """
-print(f"---Ejercicio nº 5: conversor de temperaturas Cº Fº Kº")
-grados_celsius = float(input("Introduce una temperatura en grados Celsius: "))
-grados_kelvin = grados_celsius + 273
-grados_farenheit = ((grados_celsius * 9) /5) + 32
-grados_cochinillos =  ((grados_celsius * 22) + 123) - (2 + (grados_celsius / grados_farenheit))
-grados_alubianos = (grados_celsius - grados_cochinillos - grados_farenheit) / grados_kelvin
-print(f'A {grados_celsius}º corresponden:')
-print(f'{grados_farenheit:.2f}º Farenheit - {grados_kelvin:<5.2f}º Kelvin')
-print(f'{grados_cochinillos:.2f}º Cochinillos - {grados_alubianos:<5.2f}º Alubianos')
-print(f"{linea}")
+def ejercicio_05(grados_celsius):
+    print(f"---Ejercicio nº 5: conversor de temperaturas Cº Fº Kº")
+    #grados_celsius = float(input("Introduce una temperatura en grados Celsius: "))
+    grados_kelvin = grados_celsius + 273
+    grados_farenheit = ((grados_celsius * 9) /5) + 32
+    grados_cochinillos =  ((grados_celsius * 22) + 123) - (2 + (grados_celsius / grados_farenheit))
+    grados_alubianos = (grados_celsius - grados_cochinillos - grados_farenheit) / grados_kelvin
+    print(f'A {grados_celsius}º corresponden:')
+    print(f'{grados_farenheit:.2f}º Farenheit - {grados_kelvin:<5.2f}º Kelvin')
+    print(f'{grados_cochinillos:.2f}º Cochinillos - {grados_alubianos:<5.2f}º Alubianos')
+    print(f"{linea}")
+    return(grados_farenheit,grados_kelvin)
 
 
 #6. Realiza un programa que lea pulgadas y pase a pies, codos, varas, índi es, orejas y jamones.
@@ -111,61 +119,69 @@ indices = pulgadas * 0.85
 orejas = pulgadas * 0.99 + 2
 jamones = varas - 12 + (indices * orejas) / codos
 """
-print(f"---Ejercicio nº 6: conversor de medidas pulgadas")
-pulgadas = float(input("Introduce una medida en pulgadas: "))
-pies = pulgadas / 15
-codos = pies / 1.3
-varas = codos / 1.23
-indices = pulgadas * 0.85
-orejas = (pulgadas * 0.99) + 2
-jamones = ((varas - 12) + (indices * orejas)) / codos
-print(f'A {pulgadas} corresponden: ')
-print(f'pies: {pies:.2f} - codos: {codos:.2f}')
-print(f'varas: {varas:.2f} - indices: {indices:.2f}')
-print(f'orejas: {orejas:.2f} - jamones: {jamones:.2f}')
-print(f"{linea}")
-
+def ejercicio_06(pulgadas):
+    print(f"---Ejercicio nº 6: conversor de medidas pulgadas")
+    #pulgadas = float(input("Introduce una medida en pulgadas: "))
+    pies = pulgadas / 15
+    codos = pies / 1.3
+    varas = codos / 1.23
+    indices = pulgadas * 0.85
+    orejas = ((pulgadas * 0.99) + 2)
+    jamones = ((varas - 12) + (indices * orejas)) / codos
+    print(f'A {pulgadas} corresponden: ')
+    print(f'pies: {pies:.2f} - codos: {codos:.2f}')
+    print(f'varas: {varas:.2f} - indices: {indices:.2f}')
+    print(f'orejas: {orejas:.2f} - jamones: {jamones:.2f}')
+    print(f"{linea}")
+    return(round(pies,2), round(codos,2), round(varas,2), round(indices,2), round(orejas,2), round(jamones,2))
 
 #7. Realiza un programa que calcule el área de un triángulo y un cuadrado.
-print(f"---Ejercicio nº 7: área de un cuadrado y un triángulo")
-lado = float(input("Introduce un número que sea el lado de un cuadrado y de un triángulo equilátero: "))
-area_cuadrado = lado * lado # área del cuadrado = lado * lado
-# Área del triángulo 
-# Fórmula para calcular el area del triángulo = Raiz de 3 / 4 * lado al cuadrado
-area_triangulo = (math.sqrt(3)/4) * pow(lado,2)
-print(f'El área del cuadrado es: {area_cuadrado:.2f}\nEl área del triángulo es: {area_triangulo:.2f}')
-print(f"{linea}")
+def ejercicio_07(lado):
+    print(f"---Ejercicio nº 7: área de un cuadrado y un triángulo")
+    #lado = float(input("Introduce un número que sea el lado de un cuadrado y de un triángulo equilátero: "))
+    area_cuadrado = lado * lado # área del cuadrado = lado * lado
+    # Área del triángulo 
+    # Fórmula para calcular el area del triángulo = Raiz de 3 / 4 * lado al cuadrado
+    area_triangulo = (math.sqrt(3)/4) * pow(lado,2)
+    print(f'El área del cuadrado es: {area_cuadrado:.2f}\nEl área del triángulo es: {area_triangulo:.2f}')
+    print(f"{linea}")
+    return(round(area_cuadrado,2),round(area_triangulo,2))
 
 
 
 #8. Pide al usuario su peso (kg) y estatura (m), calcula el Índice de Masa Corporal (IMC = peso / estatura²) y muestra el resultado redondeado a 2 decimales en el formato:
 #Tu índice de masa corporal es <imc> redondeado con dos decimales.
-print(f"---Ejercicio nº 8: cálculo del IMC")
-peso = (float(input("Introduce tu peso en kg: ")))
-estatura = (float(input("Introduce tu altura en metros (p.e: 1.79): ")))
-imc = peso / pow(estatura,2)
-print(f'Para un peso de {peso:.2f} kg y una estatura de {estatura:.2f} metros\ncorresponde una IMC de: {imc:.2f}')
-print(f"{linea}")
-
+def ejercicio_08(peso, estatura):
+    print(f"---Ejercicio nº 8: cálculo del IMC")
+    #peso = (float(input("Introduce tu peso en kg: ")))
+    #estatura = (float(input("Introduce tu altura en metros (p.e: 1.79): ")))
+    imc = peso / pow(estatura,2)
+    print(f'Para un peso de {peso:.2f} kg y una estatura de {estatura:.2f} metros\ncorresponde una IMC de: {imc:.2f}')
+    print(f"{linea}")
+    return(round(imc,2))
 
 
 #9. Solicita dos números enteros al usuario y muestra el cociente y el resto de su división en el formato:
 #<n> entre <m> da un cociente <c> y un resto <r>.
-print(f"---Ejercicio nº 9: división entera")
-dividendo = int(input("Introduce el dividendo (número entero):"))
-divisor = int(input("Introduce el divisor (número entero):"))
-resultado_division_entera = divmod(dividendo, divisor)
-print(f'{resultado_division_entera}')
-print(f"{linea}")
+def ejercicio_09(dividendo, divisor):
+    print(f"---Ejercicio nº 9: división entera")
+    #dividendo = int(input("Introduce el dividendo (número entero):"))
+    #divisor = int(input("Introduce el divisor (número entero):"))
+    resultado_division_entera = divmod(dividendo, divisor)
+    print(f'{resultado_division_entera}')
+    print(f"{linea}")
+    return(resultado_division_entera)
 
 
 
 #10. Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética:
 # ((3+2)/(2*5)) AL CUADRADO
-print(f"---Ejercicio nº 10: operación aritmética")
-parentesis = ((3+2)/(2*5))
-resultado_potencia = pow(parentesis,2)
-print(f'El resultado del cuadrado de ((3+2)/(2*5)) es: {resultado_potencia}') # 0.25
+def ejercicio_10():
+    print(f"---Ejercicio nº 10: operación aritmética")
+    parentesis = ((3+2)/(2*5))
+    resultado_potencia = pow(parentesis,2)
+    print(f'El resultado del cuadrado de ((3+2)/(2*5)) es: {resultado_potencia}') # 0.25
+    return resultado_potencia
 
 print(f"{linea}\nFin de los ejercicios de este bloque\n{linea}")
 
--- Modulo_1_Basico/tests/test_EJERCICIOS_01_Modulo_01.py
+++ Modulo_1_Basico/tests/test_EJERCICIOS_01_Modulo_01.py
@@ -1,34 +1,34 @@
 import pytest
 #from ejercicios.EJERCICIOS_01_Modulo_01 import *
-import ejercicios.EJERCICIOS_01_Modulo_01 as eje1
+import ejercicios.EJERCICIOS_01_Modulo_01 as mod1_eje1
 
 
-def test_ejercicio_1():
-    assert eje1.ejercicio1("Angel") == "Encantado de conocerte, Angel."
+def test_ejercicio_01():
+    assert mod1_eje1.ejercicio_01("Angel") == "Encantado de conocerte, Angel."
 
-def test_ejercicio_2():
-    pass
+def test_ejercicio_02():
+    assert mod1_eje1.ejercicio_02(50.00) == 13.89
 
-def test_ejercicio_3():
-    pass
+def test_ejercicio_03():
+    assert mod1_eje1.ejercicio_03(1000.00,2.00) == 20.00
 
-def test_ejercicio_4():
-    pass
+def test_ejercicio_04():
+    assert mod1_eje1.ejercicio_04(1,9,6) == (-0.7250827823646251, -8.274917217635375)
 
-def test_ejercicio_5():
-    pass
+def test_ejercicio_05():
+    assert mod1_eje1.ejercicio_05(40.00) == (104.00, 313.00)
 
-def test_ejercicio_6():
-    pass
+def test_ejercicio_06():
+    assert mod1_eje1.ejercicio_06(20.00) == (1.33, 1.03, 0.83, 17.00, 21.8, 350.45)
 
-def test_ejercicio_7():
-    pass
+def test_ejercicio_07():
+    assert mod1_eje1.ejercicio_07(40.00) == (1600.00, 692.82)
 
-def test_ejercicio_8():
-    pass
+def test_ejercicio_08():
+    assert mod1_eje1.ejercicio_08(90,1.79) == (28.09)
 
-def test_ejercicio_9():
-    pass
+def test_ejercicio_09():
+    assert mod1_eje1.ejercicio_09(15,4) == (3,3)
 
 def test_ejercicio_10():
-    pass
+    assert mod1_eje1.ejercicio_10() == 0.25
