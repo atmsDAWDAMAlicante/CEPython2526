cbd247438648792e22697a38f4b5733b08f11597
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-10-20 20:07:04 +0200

    Ejercicios 3 y 4 - Fechas

--- Modulo_1_Basico/ejercicios/EJERCICIOS_02_Modulo_01_Condicionales.py
+++ Modulo_1_Basico/ejercicios/EJERCICIOS_02_Modulo_01_Condicionales.py
@@ -44,21 +44,70 @@ def ejercicio_02(tfno):
 #3. Escribir un programa que pregunte al usuario la fecha de su nacimiento en formato dd/mm/aaaa 
 # y muestra por pantalla, el día, el nombre del mes y el año. 
 # Adaptar el programa anterior para que también funcione cuando el año se introduzca con 2 o 4 dígitos.
-def ejercicio_03():
-    print(f"---Ejercicio nº 3: xxx")
+def ejercicio_03(fecha):
+    print(f"---Ejercicio nº 3: Fecha de nacimiento")
 
+    #Toma el día de los dos primeros dígitos de la fecha (ignora la barra)
+    dia = fecha[:2]
 
+    #Toma el año de los dos últimos dígitos de la fecha (ignora la barra)
+    anyo = fecha[-2:]
 
+    #Toma el mes de los dos dígitos de en medio
+    mesNumero = fecha[3:-3]
+    #Para convertir el número del mes en su equivalente en letra llama 
+    #a la función devuelveMes y le pasa el número de mes
+    mes = devuelveMes(int(mesNumero))
     print(f"{linea}")
+    return f'{dia} de {mes} de {anyo}'
 
+
+def devuelveMes(num):
+    #La función devuelveMes recibe el número de mes y devuelve el string correspondiente
+    #a la posición de la tupla (número de mes - 1)
+    mesLetras = ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto", \
+                 "Septiembre", "Octubre", "Noviembre", "Diciembre")
+    return f'{mesLetras[num- 1]}'
 
 #4. Ampliar el programa anterior para que acepte tres idiomas.
-def ejercicio_04():
-    print(f"---Ejercicio nº 4: xxx")
+def ejercicio_04(fecha, lengua):
+    print(f"---Ejercicio nº 4: Fecha de nacimiento entres idiomas")
+    #Toma el día de los dos primeros dígitos de la fecha (ignora la barra)
+    dia = fecha[:2]
 
+    #Toma el año y el mes en función de la longitud del string que recibe la función
+    #por parámetro. Si es mayor que diez caracteres se considera que el año tiene 4 dígitos
+    if (len(fecha)<10):
+        anyo = fecha[-2:]
+        mesNumero = fecha[3:-3]
+    else:
+        anyo = fecha[-4:]
+        mesNumero = fecha[3:-5]
 
+    
 
+    mes = devuelveMesLan(int(mesNumero), int(lengua))
+    print(f'{dia} de {mes} de {anyo}')
     print(f"{linea}")
+    return f'{dia} de {mes} de {anyo}'
+
+
+def devuelveMesLan(num, lengua):
+    #La función devuelveMes recibe el número de mes y devuelve el string correspondiente
+    #a la posición de la tupla (número de mes - 1)
+    #La tupla se rellena con los valores correspondientes en función del segundo parámetro
+    #que se recibe y que determina el idioma: español, inglés o alemán
+    if (lengua == 0):
+        mesLetras = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto", \
+                 "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+    elif (lengua == 1):     
+        mesLetras = ["January", "February", "March", "April", "May", "June", "July", "August", \
+                     "September", "October", "November", "December"]
+    else:
+        mesLetras = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", \
+                     "September", "Oktober", "November", "Dezember"]
+    return f'{mesLetras[num- 1]}' # sólo devuelve el mes en el idioma solicitado
+
 
 
 #5. Escribir un programa que almacene la cadena de caracteres contraseña en una variable, 
@@ -66,7 +115,7 @@ def ejercicio_04():
 # si la contraseña introducida por el usuario coincide con la guardada en la variable 
 # sin tener en cuenta mayúsculas y minúsculas.
 def ejercicio_05():
-    print(f"---Ejercicio nº 5: xxx")
+    print(f"---Ejercicio nº 5: Password")
 
 
 
@@ -82,7 +131,14 @@ def ejercicio_05():
 # Escribir un programa que pregunte al usuario su nombre y edad, 
 # y muestre por pantalla el grupo que le corresponde.
 def ejercicio_06():
-    print(f"---Ejercicio nº 6: xxx")
+    print(f"---Ejercicio nº 6: Grupos de alumnos")
+
+
+#Prueba de los ejercicios
 ejercicio_01("aDoLfO")
 ejercicio_02("+34-913724710-56")
+ejercicio_03("09/07/70")
+ejercicio_04("09/07/70", 1)
+ejercicio_04("09/07/1970", 0)
+
 print(f"{linea}\nFin de los ejercicios de este bloque\n{linea}")
--- Modulo_1_Basico/tests/test_EJERCICIOS_02_Modulo_01_Condicionales.py
+++ Modulo_1_Basico/tests/test_EJERCICIOS_02_Modulo_01_Condicionales.py
@@ -3,18 +3,17 @@ import ejercicios.EJERCICIOS_02_Modulo_01_Condicionales as mod2_eje1
 
 
 def test_ejercicio_01():
-    pass
     assert mod2_eje1.ejercicio_01("Angel") == ["ANGEL", "angel", "Angel"]
-
 
 def test_ejercicio_02():
     assert mod2_eje1.ejercicio_02("+34-913724710-56") == "913724710"
 
 def test_ejercicio_03():
-    pass
+    assert mod2_eje1.ejercicio_03("09/07/70") == "09 de Julio de 70"
+
 
 def test_ejercicio_04():
-    pass
+    assert mod2_eje1.ejercicio_04("09/07/1970",1) == "09 de July de 1970"
 
 def test_ejercicio_05():
     pass
