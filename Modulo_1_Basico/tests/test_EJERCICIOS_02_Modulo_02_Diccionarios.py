aa854fce15f826b7d9e38a0ed2d764e5804d4104
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-10-28 18:09:51 +0100

    Plantilla tests Modulos 1 y 2 de la Unidad 2

--- Modulo_1_Basico/tests/test_EJERCICIOS_02_Modulo_02_Diccionarios.py
+++ Modulo_1_Basico/tests/test_EJERCICIOS_02_Modulo_02_Diccionarios.py
@@ -1,2 +1,40 @@
 import pytest
 import ejercicios.EJERCICIOS_02_Modulo_02_Diccionarios as mod2_eje2
+
+def test_ejercicio01():
+    pass
+    #assert.mod2_eje2.ejercicio01() == ""
+
+def test_ejercicio02():
+    pass
+
+def test_ejercicio03():
+    pass
+
+
+def test_ejercicio04():
+    pass
+
+def test_ejercicio05():
+    pass
+
+
+def test_ejercicio06():
+    pass
+
+def test_ejercicio07():
+    pass
+
+
+def test_ejercicio08():
+    pass
+
+def test_ejercicio09():
+    pass
+
+
+def test_ejercicio10():
+    pass
+
+
+
