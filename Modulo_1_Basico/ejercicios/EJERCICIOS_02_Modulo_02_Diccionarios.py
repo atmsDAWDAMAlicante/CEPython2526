15fc5a3a2dfee991221d70e715fc27b7535f6389
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-10-30 19:15:47 +0100

    Ejercicio 1 - Crear diccionario con la Selección Nacional

--- Modulo_1_Basico/ejercicios/EJERCICIOS_02_Modulo_02_Diccionarios.py
+++ Modulo_1_Basico/ejercicios/EJERCICIOS_02_Modulo_02_Diccionarios.py
@@ -9,8 +9,40 @@ print(f"{linea}\nInicio de los ejercicios de este bloque\n{linea}")
 
 
 #1. Enunciado
-print(f"---Ejercicio nº 1: xxx")
+print(f"---Ejercicio nº 1: Diccionario de titulares")
+#. El 11 de julio de 2010 la selección española de fútbol gano su primer y único mundial. La
+#alineación titular fue la siguiente:
+'''
+1 POR Iker Casillas Capitán
+15 DEF Sergio Ramos
+3 DEF Gerard Piqué
+5 DEF Carles Puyol
+11 DEF Joan Capdevila
+14 MED Xabi Alonso
+16 MED Sergio Busquets
+8 MED Xavi Hernández
+18 MED Pedro Rodríguez
+6 MED Andrés Iniesta
+7 DEL David Villa
+'''
+#Crea un diccionario al que llamaremos titulares. Los elementos de dicho diccionario serán
+#los once jugadores que comenzaron el partido, añadidos en el orden mostrado en la tabla
+#anterior, siendo el número de su dorsal el índice y su nombre el valor asociado.
 
+# En principio creo el diccionario como indica el enunciado, siendo la clave el dorsal y el nombre el valor
+titulares = {
+1: "Iker Casillas",
+15: "Sergio Ramos",
+3: "Gerard Piqué",
+5: "Carles Puyol",
+11: "Joan Capdevila",
+14: "Xabi Alonso",
+16: "Sergio Busquets",
+8: "Xavi Hernández",
+18: "Pedro Rodríguez",
+6: "Andrés Iniesta",
+7: "David Villa"
+}
 
 
 print(f"{linea}")
