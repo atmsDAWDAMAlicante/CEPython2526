99e08c92ccafbd9f81b374bc35438aea52072386
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-10-31 12:43:45 +0100

    Preparación archivos de ejercicios entregables 9 y 11

--- Modulo_1_Basico/ejercicios/EJ_ENTREGABLES_02_Modulo_01_Condicionales/ejer_09.py
+++ Modulo_1_Basico/ejercicios/EJ_ENTREGABLES_02_Modulo_01_Condicionales/ejer_09.py
@@ -1,2 +1,43 @@
 #EJERCICIOS NTREGABLES UD 02 - Modulo 01 - Condicionales
 #Ejercicio nº 9
+# ALUMNO: ANGEL TOMÁS MORENO SENÉN
+
+#Enunciado: Escribe un programa que en primer lugar pregunte si se
+#quiere calcular el área de untriángulo o la de un círculo.
+#Si se contesta que se quiere calcular el área de un triángulo, el programa tiene que pedir
+#entonces la base y la altura y escribir el área.
+#Si se contesta que se quiere calcular el área de un círculo, el programa tiene que pedir
+#entonces el radio y escribir el área.
+#En ambos casos el programa debe ser capaz de calcular y mostrar el resultado adecuado.
+'''
+Cálculo de áreas - Elige una figura geométrica:
+    a) Triángulo
+    b) Círculo
+    ¿Qué figura quieres calcular (Escribe T o C)? T
+        Escribe la base: 3
+        Escribe la altura: 5.5
+    Un triángulo de base 3.0 y altura 5.0 tiene un área de 8.25
+Cálculo de áreas - Elige una figura geométrica:
+    a) Triángulo
+    b) Círculo
+    ¿Qué figura quieres calcular (Escribe T o C)? C
+        Escribe el radio: 2
+        Un círculo de radio 2.0 tiene un área de 12.566370614359172
+'''
+
+
+
+
+
+
+
+
+
+
+'''
+Código del test con pytest:
+def test_entregable_09():
+    assert entregable_ejer_09.ejercicio_09(1,-2,2) == "Sin solución real"
+
+
+'''
--- Modulo_1_Basico/ejercicios/EJ_ENTREGABLES_02_Modulo_01_Condicionales/ejer_11.py
+++ Modulo_1_Basico/ejercicios/EJ_ENTREGABLES_02_Modulo_01_Condicionales/ejer_11.py
@@ -1,2 +1,54 @@
 #EJERCICIOS NTREGABLES UD 02 - Modulo 01 - Condicionales
-#Ejercicio nº 11
+#Ejercicio nº 11 
+# ALUMNO: ANGEL TOMÁS MORENO SENÉN# 
+
+# Enunciado:
+'''
+Realiza un programa que pida:
+    • El nombre del usuario
+    • Su edad
+    • Su ingreso anual (€)
+El programa debe aplicar las siguientes reglas:
+    1. Si la edad es menor de 18 → no paga impuestos.
+    2. Si tiene entre 18 y 65 años:
+        o Si gana menos de 15.000 €, paga un 5 % de impuestos.
+        o Entre 15.000 € y 30.000 €, paga un 10 %.
+        o Entre 30.000 € y 60.000 €, paga un 20 %.
+        o Más de 60.000 €, paga un 30 %.
+    3. Si tiene más de 65 años, se le aplica un descuento del 50 % sobre el impuesto
+    correspondiente.
+    4. Si su nombre empieza por “A” o “a”, obtiene un bono adicional de -2 % (descuento).
+    5. El programa mostrará:
+        El impuesto base aplicado
+        El descuento (si procede)
+        El impuesto final a pagar
+        Un mensaje personalizado con su nombre y edad.
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Código del test con pytest:
+def test_entregable_11():
+    assert entregable_ejer_011.ejercicio_011(1,-2,2) == "Sin solución real"
+
+
+'''
