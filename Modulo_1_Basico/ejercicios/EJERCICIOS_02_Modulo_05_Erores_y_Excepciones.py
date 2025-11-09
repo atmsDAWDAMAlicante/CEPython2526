c572f52858e84d7d499fcdd506d75835b35179e6
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-11-09 16:26:09 +0100

    Ejercico 5: archivos

---
+++ Modulo_1_Basico/ejercicios/EJERCICIOS_02_Modulo_05_Erores_y_Excepciones.py
@@ -0,0 +1,144 @@
+# EJERCICIOS - 02 - Modulo - 05 - Errores y Excepciones
+
+import os
+
+linea = "===================================="
+os.system('cls')
+print(f"{linea}\nInicio de los ejercicios de este bloque\n{linea}")
+
+
+#1. Enunciado: 
+
+
+def ejercicio_01(num):
+    print(f"---Ejercicio nº 1: Dxxx")
+
+
+
+
+
+#2. Enunciado: 
+
+
+def ejercicio_02(num):
+    print(f"---Ejercicio nº 2: Dxxx")
+
+
+
+
+
+#3. Enunciado: 
+
+
+def ejercicio_03(num):
+    print(f"---Ejercicio nº 3: Dxxx")
+
+
+
+
+
+#4. Enunciado: 
+
+
+def ejercicio_04(num):
+    print(f"---Ejercicio nº 4: Dxxx")
+
+
+
+
+
+
+#5. Enunciado: 
+
+
+def ejercicio_05(num):
+    print(f"---Ejercicio nº 5: Dxxx")
+
+
+
+
+
+
+#6. Enunciado: 
+
+
+def ejercicio_06(num):
+    print(f"---Ejercicio nº 6: Dxxx")
+
+
+
+
+
+
+#7. Enunciado: 
+
+
+def ejercicio_07(num):
+    print(f"---Ejercicio nº 7: Dxxx")
+
+
+
+
+
+
+#8. Enunciado: 
+
+
+def ejercicio_08(num):
+    print(f"---Ejercicio nº 8: Dxxx")
+
+
+
+
+
+
+#9. Enunciado: 
+
+
+def ejercicio_09(num):
+    print(f"---Ejercicio nº 9: Dxxx")
+
+
+
+#10. Enunciado: 
+
+
+def ejercicio_10(num):
+    print(f"---Ejercicio nº 10: Dxxx")
+
+
+#11. Enunciado: 
+
+
+def ejercicio_11(num):
+    print(f"---Ejercicio nº 11: Dxxx")
+
+
+
+#12. Enunciado: 
+
+
+def ejercicio_12(num):
+    print(f"---Ejercicio nº 12: Dxxx")
+
+
+
+
+
+#13. Enunciado: 
+
+
+def ejercicio_13(num):
+    print(f"---Ejercicio nº 13: Dxxx")
+
+
+
+#14. ENTREGABLE - Enunciado: 
+
+
+def ejercicio_14(num):
+    print(f"---Ejercicio nº 14: Dxxx")
+
+
+
+print(f"{linea}\nFin de los ejercicios de este bloque\n{linea}")
