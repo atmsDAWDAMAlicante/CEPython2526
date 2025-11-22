b87ddb58d6950b91ff422c88e61a64aee2cc0985
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-11-22 13:46:07 +0100

    Ejercicio del bucle while preparado para entregar

--- Modulo_1_Basico/ejercicios/EJ_ENTREGABLES_02_Modulo_04_While/ejer_14.py
+++ Modulo_1_Basico/ejercicios/EJ_ENTREGABLES_02_Modulo_04_While/ejer_14.py
@@ -9,107 +9,117 @@ import os
 # para limpiar la terminal
 os.system('cls')
 
-import random
+import random # para generar los números aleatorios
 
-
-print(f"---Ejercicio nº 14: Juego de Dados entregable")
+# 1. Variables globales
 
 otro_numero = "s" # Variable para mantenerse en el juego
 
-# Variables de los dados inicializadas a 0
+# 1.1 Variables de los dados inicializadas a 0
 dado_Jugador1 = 0
 dado_Jugador2 = 0
 puntuacion_Jugador1 = 0 # Variable que acumula la puntuación del jugador 1
 puntuacion_Jugador2 = 0 # Variable que acumula la puntuación del jugador 1
+
+# 1.2 Variables booleanas de los jugadores
 sigue_jugando_Jugador1 = True
 sigue_jugando_Jugador2 = True
 
-# Variables de los strings
+# 1.3 Variables de los strings
 nueva_tirada = "Para generar un nuevo número pulsa S o s, otra tecla para terminar: "
 vencedor = ""
 linea = "****************************************************************" 
 otro_numero = "s" # Variable para mantenerse en el juego
 
+# 2. La función que mantiene la partida con un bucle while
 
-# Bucle que se repite mientras no abandonan los dos jugadores
+def partida_de_dados():
+    # Problema inesperado con las variables globales dentro de la función
+    # Solución, declarar las variables como globales
+    # Esto sucede porque las variables se van a modificar dentro de la función
+    global sigue_jugando_Jugador1
+    global sigue_jugando_Jugador2
+    global puntuacion_Jugador1
+    global puntuacion_Jugador2
 
-# Estas condiciones del bucle while no funcionan bien:
-#while (sigue_jugando_Jugador1 == True or sigue_jugando_Jugador2 == True) and (puntuacion_Jugador1 <=21 or puntuacion_Jugador2 <= 21): 
-#while (sigue_jugando_Jugador1 == True) or (sigue_jugando_Jugador2 == True): #and (puntuacion_Jugador1 <=21 or puntuacion_Jugador2 <= 21):
+    # Bucle while que mantiene la partida mientras no salga los jugadores
+    while (sigue_jugando_Jugador1 == True or sigue_jugando_Jugador2 == True):
 
-# Esta de abajo casi lo hace bien:
-# while (sigue_jugando_Jugador1 == True or sigue_jugando_Jugador2 == True) and (puntuacion_Jugador1 <=21 or puntuacion_Jugador2 <= 201): 
-
-# Esta lo hace mejor, pero no del todo bien:
-#while ((sigue_jugando_Jugador1 == True or sigue_jugando_Jugador2 == True) and (puntuacion_Jugador1 <=21 or puntuacion_Jugador2 <= 21)): 
-
-# Solución: seguir con la idea original de los ejercicios anteriores y evaluar, inmediatamente después, el valor de la puntuación en un if
-while (sigue_jugando_Jugador1 == True or sigue_jugando_Jugador2 == True):
-
-    # If que evalúa si la puntuación supera 21 puntos
-    # Puesto aquí, al principio, evita que el programa pregunte por una segunda ronda de tirada de dados
-    if (puntuacion_Jugador1 >=21) or (puntuacion_Jugador2 >=21):
-        break
-        sigue_jugando_Jugador1 = False
-        sigue_jugando_Jugador2 = False
-
-    # Bloque de preguntas a los jugadores
-    # Jugador 1
-    if (sigue_jugando_Jugador1 == True):
-        otro_numero = input(f"Jugador 1: {nueva_tirada}").lower()
-        if (otro_numero != "s"):
-            print("Sale el Jugador 1")
+        # A) If que evalúa si la puntuación supera 21 puntos
+        # Puesto aquí, al principio, evita que el programa pregunte por una segunda 
+        # ronda de tirada de dados
+        if (puntuacion_Jugador1 >=21) or (puntuacion_Jugador2 >=21):
+            break
             sigue_jugando_Jugador1 = False
-        else:
-            print("Nueva tirada del jugador 1")
-            # Se genera la tirada de los dados del jugador 1: número aleatorio
-            dado_Jugador1 = random.randint(1,6)
-            # Se actualiza la puntuación del jugador 1
-            puntuacion_Jugador1 += dado_Jugador1
-            # Se muestra la puntuación del jugador 1
-            print(f"Primer jugador: Tirada actual: {dado_Jugador1} Total acumulado: {puntuacion_Jugador1}")
-
-
-    # Jugador 2
-    if (sigue_jugando_Jugador2 == True):
-        otro_numero = input(f"Jugador 2: {nueva_tirada}").lower()
-        if (otro_numero != "s"):
-            print("Sale el jugador 2")
             sigue_jugando_Jugador2 = False
-        else:
-            print("Nueva tirada del jugador 2")
-            # Se genera la tirada de los dados del jugador 2: número aleatorio
-            dado_Jugador2 = random.randint(1,6)
-            # Se actualiza la puntuación del jugador 2
-            puntuacion_Jugador2 += dado_Jugador2
-            # Se muestra la puntuación del jugador 12
-            print(f"Segundo jugador: Tirada actual: {dado_Jugador2} Total acumulado: {puntuacion_Jugador2}")
+
+        # B) Bloque de preguntas a los jugadores si siguen jugando
+        # Jugador 1
+        if (sigue_jugando_Jugador1 == True):
+            otro_numero = input(f"Jugador 1: {nueva_tirada}").lower()
+            if (otro_numero != "s"):
+                print("Sale el Jugador 1") # Informa que abandona el juego
+                sigue_jugando_Jugador1 = False # Se actualiza la variable para no preguntarle más
+            else:
+                print("Nueva tirada del jugador 1")
+                # Se genera la tirada de los dados del jugador 1: número aleatorio
+                dado_Jugador1 = random.randint(1,6)
+                # Se actualiza la puntuación del jugador 1
+                puntuacion_Jugador1 += dado_Jugador1
+                # Se muestra la puntuación del jugador 1
+                print(f"Primer jugador: Tirada actual: {dado_Jugador1} Total acumulado: {puntuacion_Jugador1}")
 
 
-print(linea)
-#Cuando salen se hace el recuento final en varios bloques condicionales
+        # Jugador 2
+        if (sigue_jugando_Jugador2 == True):
+            otro_numero = input(f"Jugador 2: {nueva_tirada}").lower()
+            if (otro_numero != "s"):
+                print("Sale el jugador 2") # Informa que abandona el juego
+                sigue_jugando_Jugador2 = False # Se actualiza la variable para no preguntarle más
+            else:
+                print("Nueva tirada del jugador 2")
+                # Se genera la tirada de los dados del jugador 2: número aleatorio
+                dado_Jugador2 = random.randint(1,6)
+                # Se actualiza la puntuación del jugador 2
+                puntuacion_Jugador2 += dado_Jugador2
+                # Se muestra la puntuación del jugador 12
+                print(f"Segundo jugador: Tirada actual: {dado_Jugador2} Total acumulado: {puntuacion_Jugador2}")
+        # Finalizado el bucle se llama a una función para imprimir el resultado
+        #recuento_final(puntuacion_Jugador1, puntuacion_Jugador2)
+        recuento_final()
 
-#Primera condición: si han superado los 21 puntos: pierden los dos
-if (puntuacion_Jugador1 > 21 and puntuacion_Jugador2 > 21):
-    print(f"Han perdido los dos: Resultado final: Jugador1: {puntuacion_Jugador1} – Jugador2: {puntuacion_Jugador2}")
+def recuento_final():
+# Como las variables de la puntuación son globales no hace falta pasarlas
+#def recuento_final(puntuacion_Jugador1, puntuacion_Jugador2):
 
-#Segunda condición: empate
-elif (puntuacion_Jugador1 == puntuacion_Jugador2) and (puntuacion_Jugador1 <= 21):
+    #A continuación se hace el recuento final en varios bloques condicionales
+
+    #Primera condición: si han superado los 21 puntos: pierden los dos
+    if (puntuacion_Jugador1 > 21 and puntuacion_Jugador2 > 21):
+        print(f"Han perdido los dos: Resultado final: Jugador1: {puntuacion_Jugador1} – Jugador2: {puntuacion_Jugador2}")
+
+    #Segunda condición: empate
+    elif (puntuacion_Jugador1 == puntuacion_Jugador2) and (puntuacion_Jugador1 <= 21):
         print(f"Empate: Resultado final: Jugador1: {puntuacion_Jugador1} – Jugador2: {puntuacion_Jugador2}")
 
-#Tercera condición: el resto de supuestos
-# IMPORTANE:
-# UNA DISYUNTIVA EN UN SOLO BLOQUE ENTRE PARÉNTESIS: Gana el jugador uno
-    #a) en la situación normal que tiene más puntos que el jugador 2
-        # O
-    #b) pase lo que pase, tenga los puntos que tenga, el jugador 2 supera los 21 puntos
+    #Tercera condición: el resto de supuestos
+    # IMPORTANE:
+    # UNA DISYUNTIVA EN UN SOLO BLOQUE ENTRE PARÉNTESIS: Gana el jugador uno
+        #a) en la situación normal que tiene más puntos que el jugador 2
+            # O
+        #b) pase lo que pase, tenga los puntos que tenga, el jugador 2 supera los 21 puntos
 
-# Y, ADEMÁS, se tiene que producir siempre la condición que el jugador 1 no supere los 21 puntos para ganar
-else:
-    if ((puntuacion_Jugador1 > puntuacion_Jugador2) or (puntuacion_Jugador2 > 21)) and (puntuacion_Jugador1 <=21):
-        vencedor = "Primer jugador"
-    # En el resto de los casos, gana el jugador 2
+    # Y, ADEMÁS, se tiene que producir siempre la condición que el jugador 1 no supere los 21 puntos para ganar
     else:
-        vencedor = "Segundo jugador"
-    #Impresión del resultado final
-    print(f"Vencedor: {vencedor}. Resultado final: Jugador1: {puntuacion_Jugador1} – Jugador2: {puntuacion_Jugador2}")
+        if ((puntuacion_Jugador1 > puntuacion_Jugador2) or (puntuacion_Jugador2 > 21)) and (puntuacion_Jugador1 <=21):
+            vencedor = "Primer jugador"
+        # En el resto de los casos, gana el jugador 2
+        else:
+            vencedor = "Segundo jugador"
+        #Impresión del resultado final
+        print(f"Vencedor: {vencedor}. Resultado final: Jugador1: {puntuacion_Jugador1} – Jugador2: {puntuacion_Jugador2}")
+
+
+#Inicio del programa
+print(f"---Ejercicio nº 14: Juego de Dados entregable")
+partida_de_dados()
