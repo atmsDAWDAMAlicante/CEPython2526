225e12304364eec3b00f1e8ab8a9f3b491c3becf
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-11-22 13:09:33 +0100

    Ejercicio 5 de las excepciones preparado para la entrega

--- Modulo_1_Basico/ejercicios/EJ_ENTREGABLES_02_Modulo_05_Errores_y_Excepciones/ejer_05.py
+++ Modulo_1_Basico/ejercicios/EJ_ENTREGABLES_02_Modulo_05_Errores_y_Excepciones/ejer_05.py
@@ -21,26 +21,50 @@ import os
 # para limpiar la terminal
 os.system('cls')
 
+#1. Variables globales
+elementos = [1, 'pie', -2]
 
-def agregar_una_vez(lista, elemento): #El ejercicio 5
-    #print(f"---Ejercicio nº 5: Agregar elemento no repetido")
-    # Se recibe la lista y se forma la primera parte del fstring
-    try:
-        resultado = f"La lista actual es: {lista}\nValor que quieres añadir a la lista: {elemento}"
-        # Se busca el duplicado
-        # for i in lista: --> me puedo ahorrar el bucle con un in
-        if (elemento in lista): # Si el elemento está en la lista
+# 2. Funciones
+
+# 2.1 Función principal del programa
+
+def ejercicio_05(): #Primera función: pide el elemento.
+
+    # Se repite indefinidamente
+    while True:
+        elemento = input(f"Introduce un elemento no repetido:\n>>>>>  ") # Pide el elemento
+        #resultado = f"La lista actual es: {elementos}\nValor que quieres añadir a la lista: {elemento}"
+        resultado = "" # Se actualiza la cadena resultado a ""
+        try: # Primero intenta converitr el elemento a un entero
+            elemento = int(elemento) # Primero se intenta convertir el número a entero
+            # Si lo consigue lo envía la funcion agregar_una_vez como ENTERO para añadirlo
+            resultado = agregar_una_vez(elemento)
+        except ValueError: # Si se lanza el error, lo envía como STRING
+            resultado = agregar_una_vez(elemento)
+        # Las dos llamadas esperan de retorno el string que se imprime en pantalla
+        finally: # Siempre se imprime el resultado, sea el que sea
+            print(resultado)
+
+# 2.2 Función que añade el elemento si no está y devuelve un fstring con el resultado
+
+def agregar_una_vez(elemento): # Recibimos el elemento ya como entero o como string
+    try: # Primero intenta ver si está en la lista (como entero o como string)
+        if (elemento in elementos): # Si el elemento está en la lista lanza la excepción
             raise Exception
         else:
             # No está en la lista
-            lista.append(elemento) # Lo añade a la lista
-            # Se actualiza el fstring
-            resultado += f"\nLa lista actual es: {lista}"
-        return resultado
-    except Exception:
-        resultado += f"\nError: Imposible añadir elementos duplicados => {elemento}\nLa lista actual es: {lista}"
-        return resultado
+            elementos.append(elemento) # Lo añade a la lista
+            print(f"\nHas añadido {elemento}.") # Imprime que lo ha añadido
+            resultado = f"\nLa lista actual es: {elementos}" # Se actualiza el fstring
+        return resultado # retorna el  fstring para imprimirlo en el finally de la función principal
+    
+    except Exception: # Se actualiza el fstring con el error
+        resultado = f"\nError: Imposible añadir elementos duplicados => {elemento}\nLa lista actual es: {elementos}"
+        return resultado # retorna el  fstring para imprimirlo en el finally de la función principal
 
-print(agregar_una_vez([1, 'pie', -2], 10))
-print(agregar_una_vez([1, 'pie', -2], 'pie'))
+
+#Inicio del programa
+print(f"---Ejercicio nº 5: Agregar elemento no repetido")
+ejercicio_05()
+
 
