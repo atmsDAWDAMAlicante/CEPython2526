31e44231b36fea0fe5a015c55262169fd58e160c
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-12-05 18:37:26 +0100

    Ejercicios de funciones: 3 archivos py

---
+++ Modulo_1_Basico/ejercicios/EJERCICIOS_03_Modulo_01.py
@@ -0,0 +1,48 @@
+# EJERCICIOS - 03 - Modulo - 01 - Funciones I
+
+import os
+
+linea = "===================================="
+os.system('cls')
+print(f"{linea}\nInicio de los ejercicios del bloque Funciones 1\n{linea}")
+
+
+
+#1. Enunciado: Escribe el siguiente código y añade los comentarios necesarios para explicar su funcionamiento.
+print(f"---Ejercicio nº 1: Explicar código")
+
+
+print(linea)
+
+#2. Enunciado: 
+print(f"---Ejercicio nº 2: XX")
+
+
+print(linea)
+
+
+#3. Enunciado: 
+print(f"---Ejercicio nº 3: XX")
+
+
+print(linea)
+
+
+#4. Enunciado: 
+print(f"---Ejercicio nº 4: XX")
+
+
+print(linea)
+
+
+#5. Enunciado: 
+print(f"---Ejercicio nº 5: XX")
+
+
+print(linea)
+
+#6. Enunciado: 
+print(f"---Ejercicio nº 6: XX")
+
+
+print(linea)
---
+++ Modulo_1_Basico/ejercicios/EJERCICIOS_03_Modulo_02.py
@@ -0,0 +1,73 @@
+# EJERCICIOS - 03 - Modulo - 02 - Funciones II
+
+import os
+
+linea = "===================================="
+os.system('cls')
+print(f"{linea}\nInicio de los ejercicios del bloque Funciones 1\n{linea}")
+
+
+#1. Enunciado: 
+print(f"---Ejercicio nº 1: XX")
+
+
+print(linea)
+
+#2. Enunciado: 
+print(f"---Ejercicio nº 2: XX")
+
+
+print(linea)
+
+
+#3. Enunciado: 
+print(f"---Ejercicio nº 3: XX")
+
+
+print(linea)
+
+
+#4. Enunciado: 
+print(f"---Ejercicio nº 4: XX")
+
+
+print(linea)
+
+
+#5. Enunciado: 
+print(f"---Ejercicio nº 5: XX")
+
+
+print(linea)
+
+#6. Enunciado: 
+print(f"---Ejercicio nº 6: XX")
+
+
+print(linea)
+
+#7. Enunciado: 
+print(f"---Ejercicio nº 7: XX")
+
+
+print(linea)
+
+#8. Enunciado: 
+print(f"---Ejercicio nº 8: XX")
+
+
+print(linea)
+
+#9. Enunciado: 
+print(f"---Ejercicio nº 9: XX")
+
+
+print(linea)
+
+
+
+#10. Enunciado: 
+print(f"---Ejercicio nº 10: XX")
+
+
+print(linea)
