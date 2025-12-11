50f49635fa04fcbb93c3519076ddd45c2fa03da1
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-12-11 13:12:51 +0100

    Ejercicio 2: sumar tiempos

--- Modulo_1_Basico/ejercicios/EJERCICIOS_03_Modulo_02.py
+++ Modulo_1_Basico/ejercicios/EJERCICIOS_03_Modulo_02.py
@@ -14,20 +14,23 @@ print(f"{linea}\nInicio de los ejercicios del bloque Funciones 2\n{linea}")
 # horas, m minutos y s segundos. A través de una función se debe convertir cada uno de estos
 # valores a segundos. El programa mostrará como resultado la suma de los tres valores en
 # formato de segundos.
+'''
 print(f"---Ejercicio nº 1: Convertir tiempo en segundos")
 resultado = pedirNumeros(3)
 print(f'TOTAL: {convertir_a_segundos(resultado)} segundos')
 print(linea)
-
+'''
 #2. Enunciado: Modifica el programa anterior para conseguir que el resultado de la 
 # suma de los tres tiempos se muestre en formato de h:m:s
 print(f"---Ejercicio nº 2: Suma total de tiempo")
-tiempos = []
-for i in range (3):
-    tiempos.append(pedirNumeros(3))
+tiempos = [[5,57,3],[4,4,15],[0,3,59]]
+
+#for i in range (3):
+#    tiempos.append(pedirNumeros(3))
 print(tiempos)
-
-
+#for j in range (len(tiempos)):
+ #   print(f'{tiempos[j][1]}')
+suma_tiempos(tiempos)
 
 print(linea)
 
@@ -54,32 +57,6 @@ print(linea)
 
 #6. Enunciado: 
 print(f"---Ejercicio nº 6: XX")
-
-
-print(linea)
-
-#7. Enunciado: 
-print(f"---Ejercicio nº 7: XX")
-
-
-print(linea)
-
-#8. Enunciado: 
-print(f"---Ejercicio nº 8: XX")
-
-
-print(linea)
-
-#9. Enunciado: 
-print(f"---Ejercicio nº 9: XX")
-
-
-print(linea)
-
-
-
-#10. Enunciado: 
-print(f"---Ejercicio nº 10: XX")
 
 
 print(linea)
--- Modulo_1_Basico/ejercicios/EJERCICIOS_03_Modulo_02_funciones.py
+++ Modulo_1_Basico/ejercicios/EJERCICIOS_03_Modulo_02_funciones.py
@@ -18,9 +18,40 @@ def pedirNumeros(veces):
             print(f'{resultado} - veces: {veces}') 
     return resultado
 
-
+# Ejercicio 1
 def convertir_a_segundos(lista):
     horasAminutos = lista[0]*60
     minutosAsegundos= (horasAminutos*60) + (lista[1]*60)
        
     return minutosAsegundos + lista[2]
+
+
+# Ejercicio 2
+def suma_tiempos(tiempos):
+    horas = 0
+    minutos = 0
+    segundos = 0
+    
+    for t in tiempos:
+       horas += t[0]
+       minutos += t[1]
+       segundos += t[2]
+    
+    tiempo = []
+    tiempo.append(horas)
+    tiempo.append(minutos)
+    tiempo.append(segundos)
+    print(tiempo)
+    acumular_excesos(tiempo)
+
+def acumular_excesos(tiempo):
+    
+    horas, minutos, segundos = tiempo
+    tiempo_final = [horas,0,0]
+    if (segundos > 59):
+        tiempo_final[1]+= segundos//60
+        tiempo_final[2]+= segundos%60
+    if (minutos > 59):
+        tiempo_final[0]+= minutos//60
+        tiempo_final[1]+= minutos%60
+    print(f'Tiempo total: {tiempo_final[0]} h: {tiempo_final[1]} m: {tiempo_final[2]} s')
