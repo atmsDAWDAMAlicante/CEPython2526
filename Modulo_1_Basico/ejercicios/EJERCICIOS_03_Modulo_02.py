5cd7807c2b53099fca62cd978c22fef8a3487c90
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-12-11 11:08:25 +0100

    Ejercicios 2 de Funciones - con el módulo aparte

--- Modulo_1_Basico/ejercicios/EJERCICIOS_03_Modulo_02.py
+++ Modulo_1_Basico/ejercicios/EJERCICIOS_03_Modulo_02.py
@@ -1,15 +1,21 @@
 # EJERCICIOS - 03 - Modulo - 02 - Funciones II
-
+from ejercicios.EJERCICIOS_03_Modulo_02_funciones import *
 import os
+
+# EJECUCIÓN: desde el directorio raiz
+# python -m ejercicios.EJERCICIO_03_Modulo_02
 
 linea = "===================================="
 os.system('cls')
-print(f"{linea}\nInicio de los ejercicios del bloque Funciones 1\n{linea}")
+print(f"{linea}\nInicio de los ejercicios del bloque Funciones 2\n{linea}")
 
 
-#1. Enunciado: 
-print(f"---Ejercicio nº 1: XX")
-
+#1. Enunciado: Crea un programa que solicite tres valores de tiempo con el formato h:m:s. Donde h son
+# horas, m minutos y s segundos. A través de una función se debe convertir cada uno de estos
+# valores a segundos. El programa mostrará como resultado la suma de los tres valores en
+# formato de segundos.
+print(f"---Ejercicio nº 1: Convertir tiempo en segundos")
+print(aSegundos())
 
 print(linea)
 
