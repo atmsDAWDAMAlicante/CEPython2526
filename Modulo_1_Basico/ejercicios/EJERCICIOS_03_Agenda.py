e90bcc7b4e72bbb1ccd612a139923de2023d764e
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-12-14 19:43:22 +0100

    Repaso y hecha la agenda le falta mejora en los módulos

--- Modulo_1_Basico/ejercicios/EJERCICIOS_03_Agenda.py
+++ Modulo_1_Basico/ejercicios/EJERCICIOS_03_Agenda.py
@@ -10,7 +10,35 @@ os.system("cls")
 
 # Función principal: muestra y llama al menú
 def main():
-    operacion = validar_menu()
-    if (type(operacion) == int):
-        ejecutar_operacion(operacion)
+    operacion = validar_menu_principal()
+    if (operacion == 6):
+        print(f"Hasta pronto\n{fin}")
+        return
+    elif (operacion == 5):
+        print(agenda)
+    elif (operacion == 1):
+        anadir_contacto()
+    elif (operacion == 2):
+        consultar_contacto()
+    elif (operacion == 3):
+        modificar_contacto()
+    elif (operacion == 4):
+        borrar_contacto()
+    main()
+
+'''
+# ¿OTRA OPERACIÓN?
+def otra_operacion():
+    otra = input("¿Desea realizar otra operación? [S/N]: ").lower()
+    while True:
+        if (otra == "s"):
+            validar_menu_principal()
+        elif (otra == "n"):
+            print(fin)
+            break
+        else:
+            otra = input("¿Cómo dice? [S/N]: ").lower()
+'''
+
+
 main()
--- Modulo_1_Basico/ejercicios/EJERCICIOS_03_Agenda_Modulo.py
+++ Modulo_1_Basico/ejercicios/EJERCICIOS_03_Agenda_Modulo.py
@@ -24,52 +24,71 @@ agenda = {"Ana": {"telefono": "600123123", "email": "ana@example.com"},
     #6. 6. Salir
 salto = "\n"
 menu = f'MENÚ: 1. Añadir contacto - 2. Consultar contacto - 3. Modificar contacto - 4. Borrar contacto - 5. Listar todos los contactos - 6. Salir'
+fin = "==== FIN DEL PROGRAMA ===="
 
-def validar_menu():
+# VALIDACION DEL MENÚ
+def validar_menu_principal():
     print(menu)
     operacion = input("Introduce una operacion: ")
     while True:
         try:
             operacion = int(operacion)
-            if (operacion > 6) or (operacion < 1):
-                raise ValueError
-            elif (operacion == 6):
-                print("Has elegido salir. Hasta pronto.")
-                break
-            else:
+            if (operacion > 0) and (operacion < 7):
                 return operacion
+            else:
+                operacion = input("Introduce un entero entre 1-6: ")
         except ValueError:
             print(f"Operación '{operacion}' incorrecta")
             operacion = input("Introduce una operacion, PERO AHORA HAZLO BIEN: ")
-     
-        
-           
 
 
-#bifurcacion operaciones
-def ejecutar_operacion(num):
-    
-    print (f'Has elegido {type(num)} operacion {num}')
-    if (num == 1):
-        validar_menu()
-    elif (num == 2):
-        pass
-    elif (num == 3):
-        pass
-    elif (num == 4):
-        pass
-    elif (num == 5):
-        pass
-        print (agenda)
-    else:
-        pass
-        
-        
 
-'''
-def formar_diccionario():
-    tuplaA = ("Maria", "Luis", "Pepe")
-    tuplaB = (4,5,6)
-    diccionario = dict(zip(tuplaA,tuplaB))
-    print(diccionario)
-'''
+# SELECCIÓN DE OPERACIONES
+def anadir_contacto():
+    contacto = input("Introduzca el contacto: ").strip()
+    while True:
+        if (contacto in agenda.keys()):
+            print("El contacto ya está en la agenda")
+            break
+        else:
+            agenda[contacto] = {}
+            telefono = input("Introduce el telefono: ")
+            email = input("Introduce el email: ")
+            agenda[contacto] = {"telefono": telefono, "email": email}
+            print(agenda)
+            break
+
+        
+def consultar_contacto():
+    contacto = input("Introduzca el contacto: ").strip()
+    while True:
+        if (contacto not in agenda.keys()):
+            print("El contacto no existe")
+            break 
+        elif (contacto in agenda.keys()):
+            print(f'El contacto {agenda[contacto]}')
+            break
+
+def modificar_contacto():
+    contacto = input("Introduzca el contacto: ").strip()
+    while True:
+        if (contacto not in agenda.keys()):
+            print("El contacto no existe")
+            break 
+        elif (contacto in agenda.keys()):
+            print(f'El contacto {agenda[contacto]} existe:')
+            telefono = input("Introduce su nuevo teléfono: ")
+            email = input("Introduce su nuevo email: ")
+            agenda[contacto] = {"telefono":telefono, "email":email}
+            print(agenda)
+            break
+
+def borrar_contacto():
+    contacto = input("Introduzca el contacto: ").strip()
+    while True:
+        if (contacto not in agenda.keys()):
+            print("El contacto no existe")
+            break 
+        elif (contacto in agenda.keys()):
+            agenda.pop(contacto)
+            break
