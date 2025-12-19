b3306e30bd3252ea61c580dd009278513ef99be8
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-12-19 19:24:17 +0100

    Arreglo test Agenda

--- Modulo_1_Basico/tests/test_EJERCICIOS_03_Agenda.py
+++ Modulo_1_Basico/tests/test_EJERCICIOS_03_Agenda.py
@@ -1,6 +1,6 @@
 import pytest
-import ejercicios.EJERCICIOS_03_Agenda as Miagenda 
-import ejercicios.EJERCICIOS_03_Agenda_Modulo as MiModulo
+#import ejercicios.EJERCICIOS_03_Agenda as Miagenda 
+#import ejercicios.EJERCICIOS_03_Agenda_Modulo as MiModulo
 
 def test_menu():
     pass
