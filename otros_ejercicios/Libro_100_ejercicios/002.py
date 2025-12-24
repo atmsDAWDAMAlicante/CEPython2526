8ed401d1883814030919ea177ff53b8a8383f65f
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-12-24 19:31:36 +0100

    Ejercicio nº 2

---
+++ otros_ejercicios/Libro_100_ejercicios/002.py
@@ -0,0 +1,30 @@
+#ENUNCIADO Ejercicio 2: 
+# Escribir una secuencia de instrucciones de Python 
+# que permita declarar una variable llamada ch inicializándola 
+# con el valor 'hola' y luego modificar esa misma variable para 
+# que contenga el mensaje 'está bien' .
+# El programa debe mostrar el contenido de la variable en la consola 
+#después de la modificación.
+
+import os
+os.system("cls")
+
+# Modelo
+def modificar_variable(ch):
+    ch = "está bien"  # Modifica el valor de la variable ch
+    return ch
+    
+# Vista
+def mostrar_variable(ch):
+    print("Ejercicio 2:")
+    print(f"Contenido de la variable 'ch' después de la modificación: {ch}")
+
+# Controlador
+def main():
+    ch = "hola"  # Inicializa la variable ch con el valor 'hola'
+    ch = modificar_variable(ch) # Modifica la variable ch
+    mostrar_variable(ch)  # Muestra la variable ch
+
+
+if __name__ == "__main__":
+    main()
