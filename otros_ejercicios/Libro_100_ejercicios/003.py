8bf8450f20c885c837221783a4936a08f5d15fd6
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-12-24 19:40:20 +0100

    Ejercicio nº 3

---
+++ otros_ejercicios/Libro_100_ejercicios/003.py
@@ -0,0 +1,36 @@
+# ENUNCIADO Ejercicio 3: 
+# Escribir una serie de instrucciones de Python que permita declarar 2
+# variables 'x' e 'y' asignándoles respectivamente los valores 3 y 8.5, luego
+# convertir el tipo de estas variables a una cadena de caracteres.
+# Al final, el programa debe mostrar el tipo de estas variables después de
+# la conversión.
+
+import os
+os.system("cls")
+
+
+# Modelo 
+def conversion_a_cadena(x, y):
+    # Conversión de las variables a cadena
+    x = str(x)
+    y = str(y)
+    return x, y
+
+# Vista
+def mostrar_tipos(x, y):
+    print("Ejercicio 3:")
+    print(f"Tipo de la variable 'x' después de la conversión: {type(x)}")
+    print(f"Tipo de la variable 'y' después de la conversión: {type(y)}")
+
+# Controlador
+def main():
+    # Declaración de las variables x e y
+    x = 3
+    y = 8.5
+    # Llamada a la función conversión a cadena
+    x, y = conversion_a_cadena(x, y)
+    # Mostrar el tipo de las variables convertidas
+    mostrar_tipos(x, y)
+
+if __name__ == "__main__":
+    main()
