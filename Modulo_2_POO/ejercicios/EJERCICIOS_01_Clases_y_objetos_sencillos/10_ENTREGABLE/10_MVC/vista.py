8693a1ca96644a99509552119fc2a09c32bc231b
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2026-01-15 20:18:03 +0100

    Comentarios añadidos en la VISTA

--- Modulo_2_POO/ejercicios/EJERCICIOS_01_Clases_y_objetos_sencillos/10_ENTREGABLE/10_MVC/vista.py
+++ Modulo_2_POO/ejercicios/EJERCICIOS_01_Clases_y_objetos_sencillos/10_ENTREGABLE/10_MVC/vista.py
@@ -1,5 +1,5 @@
 
-
+# CLASE CON MÉTODOS ESTÁTICOS PARA REALIZAR OPERACIONES VARIAS
 
 # Introducir opciones
 class Introduccion():
@@ -13,9 +13,12 @@ class Introduccion():
 # Impresión por consola
 class Impresion():
     @staticmethod
-    def impresion_basica(texto):
+    def impresion_basica(texto): # PARA LA IMPRESIÓN DE UN STRING PASADO POR PARÁMETRO
         print(texto)
 
+    # PARA LA IMPRESIÓN DE LOS RESULTADOS DE LAS DISTINTAS OPERACIONES DE LA CLASE EMPLEADO
+
+    # PARA LA IMPRESIÓN DEL CAMBIO DE DEPARTAMENTO
     @staticmethod
     def impresion_departamentos(empresa):
         print("\n---> Distribución por departamentos:")
@@ -25,6 +28,17 @@ class Impresion():
         print("== INFORME por DEPARTAMENTOS COMPLETADO\n")
         print("-" * 10)
 
+    # PARA LA IMPRESIÓN DEL CÁLCULO INDIVIDUAL DE LA NÓMINA TRAS INTRODUCIR LAS HORAS EXTRA
+    @staticmethod
+    def mostrar_nomina_mes(plantilla):
+        print("\n---> Cálculo de nóminas:")
+        print("\n== IMPRIMIENDO NÓMINAS...")
+        for elemento in plantilla:
+            print(f'{elemento[0]} ha trabajado {elemento[1]} horas extra: salario: {elemento[2]} €')
+        print("== INFORME de NÓMINAS COMPLETADO\n")
+        
+
+    # PARA LA IMPRESIÓN DE LA INFORMACIÓN DE TODA LA EMPRESA (No está en la CLASE)
     @staticmethod
     def mostrar_info_toda_la_empresa(empresa):
         print("=" * 25)
@@ -36,12 +50,4 @@ class Impresion():
         print("== INFORME de toda la empresa COMPLETADO\n")
         print("=" * 25)
     
-    @staticmethod
-    def mostrar_nomina_mes(plantilla):
-        print("\n---> Cálculo de nóminas:")
-        print("\n== IMPRIMIENDO NÓMINAS...")
-        for elemento in plantilla:
-            print(f'{elemento[0]} ha trabajado {elemento[1]} horas extra: salario: {elemento[2]} €')
-        print("== INFORME de NÓMINAS COMPLETADO\n")
-        
        
