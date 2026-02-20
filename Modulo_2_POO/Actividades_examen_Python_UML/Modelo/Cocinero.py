af09974b78b4b8111441f0f7c93085440347c4a9
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2026-02-20 18:53:51 +0100

    Repaso atributos de la clase Cocinero

--- Modulo_2_POO/Actividades_examen_Python_UML/Modelo/Cocinero.py
+++ Modulo_2_POO/Actividades_examen_Python_UML/Modelo/Cocinero.py
@@ -1,8 +1,9 @@
-from Modelo.Trabajador import Trabajador
+from .Trabajador import Trabajador # Ruta relativa
 
 class Cocinero(Trabajador):
     def __init__(self, nombre, dni, sueldo):
-        super().__init__(nombre, dni, sueldo)
+        super().__init__(nombre, dni, sueldo) # En el diagrama de clases no tiene atributos propios
 
+    # Métodos propios de la clase Cocinero
     def preparar_pedido():
         pass
