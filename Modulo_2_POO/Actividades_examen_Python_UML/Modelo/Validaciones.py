73dfdfa06c70a106e4b1515c27c23d7aa2e6b706
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2026-02-25 18:10:21 +0100

    Validar introducción float en Validaciones

--- Modulo_2_POO/Actividades_examen_Python_UML/Modelo/Validaciones.py
+++ Modulo_2_POO/Actividades_examen_Python_UML/Modelo/Validaciones.py
@@ -3,7 +3,7 @@ import re
 class Validaciones:
 
     @staticmethod
-    def validar_menu_principal(num):
+    def validar_menu_principal(num): # Validar opción introducida del menú prinicpal (int)
         try:
             int(num)
         except ValueError:
@@ -14,8 +14,9 @@ class Validaciones:
             else:
                 return int(num)
             
+
     @staticmethod
-    def validar_numero_en_rango(num, lim):
+    def validar_numero_en_rango(num, lim): # Validar que el número introducido es entero y dentro de un rango
         try:
             int(num)
         except ValueError:
@@ -25,10 +26,20 @@ class Validaciones:
                 return int(num)
             else:
                 return -1
-        
+            
 
     @staticmethod
-    def validar_dni(dni):
+    def validar_float(num): # Validar que el número introducido es un float
+        try:
+            float(num)
+        except ValueError:
+            return -1
+        else:
+            return float(num)
+
+
+    @staticmethod
+    def validar_dni(dni): # Validar que se ha introducido un DNI de 8 números y una mayúscula
         patron_dni = r'^\d{8}[A-Z]$'
         
         if (re.match(patron_dni, dni)):
@@ -36,8 +47,9 @@ class Validaciones:
         else: 
             return False
         
+
     @staticmethod
-    def validar_password(password):
+    def validar_password(password): # Validar la contraseña, mínimo: 1 mayúscula, 1 minúscula y 1 número
         patron_password = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$"
         if (re.match(patron_password, password)):
             return True
@@ -45,5 +57,5 @@ class Validaciones:
             return False
 
     @staticmethod
-    def validar_fecha(fecha):
+    def validar_fecha(fecha): # Validar que se ha introducido una fecha correcta
         pass
