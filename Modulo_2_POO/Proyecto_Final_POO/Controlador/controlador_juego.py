57c44add50b92ca59a1b910ca6f5d2960a4588de
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2026-04-17 20:55:31 +0200

    Coordinadas las variables y objetos de las notas del cuaderno de Jupyter ¡POR FIN!

--- Modulo_2_POO/Proyecto_Final_POO/Controlador/controlador_juego.py
+++ Modulo_2_POO/Proyecto_Final_POO/Controlador/controlador_juego.py
@@ -1,11 +1,15 @@
 # CONTROLADOR QUE GESTIONA EL JUEGO
 
 # Modelo
-from Modelo.combate import PERSONAJES, obtener_personajes_para_menu, Combate
+from Modelo.gestor_personajes import PERSONAJES, Gestor_personajes
+from Modelo.combate import Combate
+from Modelo.accion import Accion, Ataque, Ataque_Cargado, Usar_Pocion
+from Modelo.jugador import Jugador
+from Modelo.enemigo import Enemigo
 
 # Vista
-from Vista.vista_cli import Vista_CLI
-
+from Vista.mensajes import Mensajes
+from Vista.vista_cli import Menus, Vista_CLI
 
 class Controlador_juego:
 
@@ -13,47 +17,93 @@ class Controlador_juego:
         self.vista = vista
 
     def iniciar_juego(self):
-
         while True:
-
             opcion = self.vista.menu_iniciar_juego()
-
             if opcion == 1:
                 self.nuevo_juego()
-
             elif opcion == 2:
                 self.guardar_partida()
-
             elif opcion == 0:
                 self.vista.imprimir_mensaje("Adiós")
                 break
-
-    def nuevo_juego(self):
-
-        # ELECCIÓN DEL JUGADOR
-        self.vista.imprimir_mensaje("Empezamos el juego: ELIJE TU JUGADOR: ")
-
-        #PRIMERA PARTE: El menú para obtener el índice del personaje que será el jugador
-        todos_los_personajes = obtener_personajes_para_menu()
-        # Aquí se recoge el índice
-        numero_jugador = self.vista.menu_elegir_jugador(todos_los_personajes)
-        # Se saca al jugador del cesto de personajes
-        resto_enemigos = PERSONAJES
-        jugador = resto_enemigos.pop(numero_jugador-1)
-        enemigo = resto_enemigos.pop(0)
-        
-        
-        self.vista.imprimir_mensaje(f'Has elegido al/a la: {jugador["nombre"]}\nAhora vas a luchar contra {enemigo["nombre"]}')
-        los_otros = "y después te enfrentarás:\n"
-        for i in resto_enemigos:
-            los_otros += f'- {i["nombre"]}\n'
-        self.vista.imprimir_mensaje(los_otros)
-
-        # ENVIO AL ADMINISTRADOR DEL JUEGO DE LOS OBJETOS JUGADOR Y ENEMIGOS
-        nueva_partida = Combate(jugador,enemigo, resto_enemigos) 
-        print(nueva_partida.__dict__)
-
-
     def guardar_partida(self):
         self.vista.imprimir_mensaje("Partida guardada")
     
+
+    def nuevo_juego(self):
+        # ELECCIÓN DEL JUGADOR
+        self.vista.imprimir_mensaje("Empezamos el juego: ELIJE TU JUGADOR: ")
+
+        
+        #PRIMERA PARTE: Obtención de los personajes:
+        personajes_partida = Gestor_personajes(PERSONAJES)
+        # Se obtiene el menú
+        todos_los_personajes = personajes_partida.obtener_personajes_para_menu_CLI()
+        # Aquí se recoge el índice del jugador desde la vista
+        numero_jugador = self.vista.menu_elegir_jugador(todos_los_personajes)
+        # Se saca al jugador del cesto de personajes
+        jugador = Jugador(**personajes_partida.obtener_jugador(numero_jugador-1))
+        enemigo = Enemigo(**personajes_partida.obtener_enemigo())
+        resto_enemigos = personajes_partida.obtener_resto()
+        self.vista.imprimir_mensaje(f'Has escogido a: {jugador.nombre}')
+        self.vista.imprimir_mensaje(f'Tu adversario es: {enemigo.nombre}')
+
+        # ENVIO AL ADMINISTRADOR DEL JUEGO DE LOS OBJETOS JUGADOR Y ENEMIGOS
+        combate = Combate(jugador,enemigo, resto_enemigos) 
+        self.bucle_combate(combate)
+
+
+    def bucle_combate(self, combate):
+        self.combate = combate
+        turno = True
+        activo = None
+        accion = None
+        contador = 0
+        while True:
+            contador += 1
+            print(f"nº {contador} - vuelta del bucle")
+            if (turno == True): # EL JUGADOR
+                acto = Menus.menu(Menus.menu_combate)
+                activo = self.combate.enemigo
+                if (acto == 1):
+                    activo.vida = Ataque().ejecucion(activo)
+                    accion = "ataque"
+                elif (acto == 2):
+                    activo.vida = Ataque_Cargado().ejecucion(activo)
+                    accion = "ataque cargado"
+                elif (acto == 3):
+                    activo.vida = Usar_Pocion().ejecucion(activo)
+                    accion = "poción"
+                elif (acto == 4):
+                    print("Partida guardada")
+                    print("Fin")
+                    break
+                else: # SALIR DEL JUGADOR
+                    print("Fin")
+                    break
+                self.combate.ejecutar_accion(accion) # Jugador acciona
+                turno = self.combate.turno_enemigo(turno) # Cambia a enemigo
+
+            else: # Turno del ejemigo
+                activo = self.combate.jugador # Cambia activo
+                self.combate.enemigo.decidir_accion() # Se lo piensa
+                activo.vida -= 1
+                turno = self.combate.turno_enemigo(turno)
+            print(f'---La vida de {activo.nombre} es {activo.vida} - TURNO: {turno}')
+            activo.estoyVivo = activo.estar_vivo(activo.vida)
+
+
+            if (self.combate.jugador.estoyVivo == False):
+                print(f"{self.combate.jugador.nombre} pierde")
+                print("Fin")
+                break
+            elif (self.combate.enemigo.estoyVivo == False):
+                print(f"{self.combate.jugador.nombre} ha ganado al {self.combate.enemigo.nombre}")
+            
+            if (len(self.combate.resto_enemigos)>0):
+                self.combate.enemigo = None
+                self.combate.enemigo = Enemigo(**self.combate.resto_enemigos.pop(0))
+            else:
+                print("Fin del juego")
+                break
+
--- Modulo_2_POO/Proyecto_Final_POO/Modelo/combate.py
+++ Modulo_2_POO/Proyecto_Final_POO/Modelo/combate.py
@@ -1,28 +1,5 @@
 
-# Modelo
-from Modelo.jugador import Jugador
-from Modelo.enemigo import Enemigo
 
-# Los personajes de Dragones y Mazmorras
-
-PERSONAJES = [
-    {"nombre": "Arquero", "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True},
-    {"nombre": "Barbaro", "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True},
-    {"nombre": "Acróbata", "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True},
-    {"nombre": "Mago",  "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True},
-    {"nombre": "Ladrona", "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True},
-    {"nombre": "Caballero", "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True}
-]
-
-def obtener_personajes_para_menu():
-    texto_menu = ""
-    # Usamos enumerate para obtener el índice y el diccionario del personaje
-    for i, personaje in enumerate(PERSONAJES):
-        # i + 1 para que el menú empiece con uno
-        # Se forma el string pero con salto de línea para que se vea más bonito
-        texto_menu += f"{i + 1} - {personaje['nombre']}\n"
-        # Ahora se devuelve un diccionario que es lo que habrá que mandar a la Vista para el menú
-    return {"texto":texto_menu,"lim":len(PERSONAJES)}
 
 
 class Combate:
@@ -35,16 +12,16 @@ class Combate:
         pass
 
     def ejecutar_accion(self, accion):
-    print(f'El {self.jugador.nombre} hace {accion}')
+        print(f'El {self.jugador.nombre} hace {accion}')
 
     def turno_enemigo(self, turno):
         turno = not turno
 
         if (turno == True):
-        print(f'Ahora le toca a {self.jugador.nombre}')
+            print(f'Ahora le toca a {self.jugador.nombre}')
 
         else:
-        print(f'Ahora le toca a {self.enemigo.nombre}')
+            print(f'Ahora le toca a {self.enemigo.nombre}')
 
 
         return turno
