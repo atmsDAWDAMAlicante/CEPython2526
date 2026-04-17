1d492f5ea90ce80ab72ed4d175152f32210db1d8
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2026-04-17 16:44:26 +0200

    Traslado a otro archivo en el modelo la gestion de los personajes: variables y la lista de personajes

---
+++ Modulo_2_POO/Proyecto_Final_POO/Modelo/gestor_personajes.py
@@ -0,0 +1,19 @@
+
+
+# Este archivo contiene :
+# La variable inicial de los personajes de la serie de TV Dragones y Mazmorras
+# otra clase: Gestion_Personajes
+
+# Variable inicial de los personajes: todos 10 de vida (inicial y total), 2 de ataque, 
+# 2 pociones (devuelven 3 puntos de vida) y están vivos (bool)
+
+
+
+PERSONAJES = [
+    {"nombre": "Arquero", "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True},
+    {"nombre": "Barbaro", "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True},
+    {"nombre": "Acróbata", "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True},
+    {"nombre": "Mago",  "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True},
+    {"nombre": "Ladrona", "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True},
+    {"nombre": "Caballero", "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True}
+]
--- Modulo_2_POO/Proyecto_Final_POO/Modelo/personaje.py
+++ Modulo_2_POO/Proyecto_Final_POO/Modelo/personaje.py
@@ -4,21 +4,7 @@
 # Se produce la herencia
 # Esta es la clase padre de Jugador y Enemigo
 
-# Este archivo también contiene :
-# La variable inicial de los personajes de la serie de TV Dragones y Mazmorras
-# otra clase: Gestion_Personajes
 
-# Variable inicial de los personajes: todos 10 de vida (inicial y total), 2 de ataque, 
-# 2 pociones (devuelven 3 puntos de vida) y están vivos (bool)
-
-PERSONAJES = [
-    {"nombre": "Arquero", "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True},
-    {"nombre": "Barbaro", "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True},
-    {"nombre": "Acróbata", "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True},
-    {"nombre": "Mago",  "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True},
-    {"nombre": "Ladrona", "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True},
-    {"nombre": "Caballero", "vida":10, "vida_max":10, "ataque":2, "pociones":2,"estoyVivo": True}
-]
 
 # Clase padre Personaje
 class Personaje:
