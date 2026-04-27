de1bd28e2ce4d037056f70cc4e01b00b7e5108eb
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2026-04-27 17:39:25 +0200

    Main preparado para GUI

--- Modulo_2_POO/Proyecto_Final_POO/main.py
+++ Modulo_2_POO/Proyecto_Final_POO/main.py
@@ -15,23 +15,34 @@ from Vista.vista_gui import Vista_GUI
 
 def main():
     os.system("cls")
-    print(Mensajes.TITULO)
-    
-    interfaz = Menus.menu(Menus.menu_interfaz)
-    
-    while True: # Bucle provisional ---- ES PROVISIONAL
-        if (interfaz == 1):
-            vista = Vista_CLI()
-            break
-        elif (interfaz == 2):
-            #vista = Vista_GUI()
-            print("¡¡¡En obras: GUI!!!")
-            break
-        else:
-            print("Adiós")
-            break
-    controlador = Controlador_juego(vista)
-    controlador.iniciar_juego()
+
+    vista_temp = Vista_CLI()
+    interfaz = vista_temp.menu_interfaz()
+
+    #Provisional
+    #print(Mensajes.TITULO)
+    #interfaz = Menus.menu(Menus.menu_interfaz)
+    #while True: # Bucle provisional ---- ES PROVISIONAL
+
+    if (interfaz == 1):
+        vista = Vista_CLI()
+        controlador = Controlador_juego(vista)
+        
+    elif (interfaz == 2):
+        vista = Vista_GUI()
+        controlador = Controlador_juego(vista)
+        
+        # Lanzar el juego sin bloquear la GUI
+        vista.root.after(100, controlador.iniciar_juego)
+        vista.iniciar()
+
+
+    else:
+        print("Adiós. No hay partida")
+
+    # Del menú CLI provisional   
+    #controlador = Controlador_juego(vista)
+    #controlador.iniciar_juego()
 
 if __name__ == "__main__":
     main()
